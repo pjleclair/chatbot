e623b831329f573606dba3821cda6fdc5fae9c9a
lompdoctor <pleclair@protonmail.com>
2023-06-18 18:46:40 -0500

    Initialize app with Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        Initial Commit
+      </div>
+      <Link href='/pages/'>New Page</Link>
+    </main>
+  )
+}
---
+++ app/pages/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function NewPage() {
+    return (
+        <>
+            <h1>New Page</h1>
+            <Link href="/">Back to Home</Link>
+        </>
+    )
+}
